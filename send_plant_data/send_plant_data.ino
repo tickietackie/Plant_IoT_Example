93e7e5933ccd30b5087315814257da170fc58287
tickietackie <kai.kuklok@gmail.com>
2021-09-04 13:37:06 +0200

    remove network scan

--- send_plant_data/send_plant_data.ino
+++ send_plant_data/send_plant_data.ino
@@ -249,10 +249,6 @@ void loop() {
     reconnect();
   }
 
-  // WiFi.scanNetworks will return the number of networks found
-  int n = WiFi.scanNetworks();
-  Serial.println("scan done");
-
   // receive measured values from DHT11
   Temperature = dht.readTemperature(); // Gets the values of the temperature
   Humidity = dht.readHumidity(); // Gets the values of the humidity
